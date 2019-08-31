76f93c84f6d3bd2e36071e649ad368f1f5419207
Xiao <xiao@scp-079.org>
2019-08-31 03:41:50 +0000

    - 0.0.2

--- plugins/functions/channel.py
+++ plugins/functions/channel.py
@@ -149,15 +149,13 @@ def forward_evidence(client: Client, message: Message, level: str, rule: str, th
             text += f"附加信息：{code(more)}\n"
         elif the_type in glovar.types["privacy"]:
             text += f"附加信息：{code('可能涉及隐私而未转发')}\n"
-        elif the_type == "gam":
-            text += f"附加信息：{code('游戏消息无法转发')}\n"
         elif the_type == "sde":
             text += f"附加信息：{code('用户要求删除其全部消息')}\n"
         elif the_type == "pur":
             text += f"附加信息：{code('群管要求删除指定消息')}\n"
 
         # Protect user's privacy
-        if the_type in glovar.types["privacy"] or the_type == "gam":
+        if the_type in glovar.types["privacy"]:
             result = send_message(client, glovar.logging_channel_id, text)
             return result
 
@@ -173,7 +171,7 @@ def forward_evidence(client: Client, message: Message, level: str, rule: str, th
                 flood_wait = True
                 wait_flood(e)
             except Exception as e:
-                logger.info(f"Forward evidence message error: {e}", exc_info=True)
+                logger.warning(f"Forward evidence message error: {e}", exc_info=True)
                 return False
 
         result = result.message_id
