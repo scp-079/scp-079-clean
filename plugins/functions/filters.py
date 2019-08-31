85f4ffb072314527c554de7dd10bda0831950f96
Xiao <xiao@scp-079.org>
2019-08-31 01:49:42 +0000

    - 0.0.2

--- plugins/functions/filters.py
+++ plugins/functions/filters.py
@@ -344,9 +344,9 @@ def is_not_allowed(client: Client, message: Message, text: str = None, image_pat
                 # Basic types messages
 
                 # Animated Sticker
-                if is_in_config(gid, "ast"):
-                    if message.sticker and message.sticker.is_animated:
-                        return "ast"
+                # if is_in_config(gid, "ast"):
+                #     if message.sticker and message.sticker.is_animated:
+                #         return "ast"
 
                 # Audio
                 if is_in_config(gid, "aud"):
