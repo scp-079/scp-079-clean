7fd44902910ea176e8701eecf8f5dd37da83b594
Xiao <xiao@scp-079.org>
2019-10-06 05:48:01 +0000

    - modify share_data

--- plugins/functions/channel.py
+++ plugins/functions/channel.py
@@ -300,8 +300,8 @@ def share_bad_user(client: Client, uid: int) -> bool:
     return False
 
 
-def share_data(client: Client, receivers: List[str], action: str, action_type: str, data: Union[bool, dict, int, str],
-               file: str = None, encrypt: bool = True) -> bool:
+def share_data(client: Client, receivers: List[str], action: str, action_type: str,
+               data: Union[bool, dict, int, str] = None, file: str = None, encrypt: bool = True) -> bool:
     # Use this function to share data in the channel
     try:
         if glovar.sender in receivers:
