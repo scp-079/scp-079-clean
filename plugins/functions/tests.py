b279b8ceee8b6cf636d52079e904cfaf6097c0ed
Xiao <xiao@scp-079.org>
2019-10-17 11:20:42 +0000

    - fix bugs

--- plugins/functions/tests.py
+++ plugins/functions/tests.py
@@ -110,10 +110,11 @@ def clean_test(client: Client, message: Message) -> bool:
 
             # Show emoji
             emoji_dict = {}
-            emoji_list = [emoji for emoji in glovar.emoji_set if emoji in text and emoji not in glovar.emoji_protect]
+            emoji_list = [emoji for emoji in glovar.emoji_set
+                          if emoji in message_text and emoji not in glovar.emoji_protect]
 
             for emoji in emoji_list:
-                emoji_dict[emoji] = text.count(emoji)
+                emoji_dict[emoji] = message_text.count(emoji)
 
             if emoji_dict:
                 text += f"{lang('emoji_total')}{lang('colon')}{code(sum(emoji_dict.values()))}\n\n"
