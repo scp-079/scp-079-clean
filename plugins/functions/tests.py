67cb7dcaf67ead0fbb1a35e031c5761bc18d387b
Xiao <xiao@scp-079.org>
2019-10-17 11:52:52 +0000

    - debug

--- plugins/functions/tests.py
+++ plugins/functions/tests.py
@@ -113,14 +113,15 @@ def clean_test(client: Client, message: Message) -> bool:
             # Show emoji
             logger.warning(message_text)
             emoji_dict = {}
-            emoji_set = {emoji for emoji in glovar.emoji_set if emoji in text and emoji not in glovar.emoji_protect}
+            emoji_set = {emoji for emoji in glovar.emoji_set
+                         if emoji in message_text and emoji not in glovar.emoji_protect}
             emoji_old_set = deepcopy(emoji_set)
             for emoji in emoji_old_set:
-                if any(emoji in emoji_old for emoji_old in emoji_old_set if emoji != emoji_old):
+                if any(emoji in emoji_old and emoji != emoji_old for emoji_old in emoji_old_set):
                     emoji_set.discard(emoji)
 
             for emoji in emoji_set:
-                emoji_dict[emoji] = text.count(emoji)
+                emoji_dict[emoji] = message_text.count(emoji)
 
             if emoji_dict:
                 text += f"{lang('emoji_total')}{lang('colon')}{code(sum(emoji_dict.values()))}\n\n"
