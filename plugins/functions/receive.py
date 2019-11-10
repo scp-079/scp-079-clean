5166208c206924ce723890620c81037239787d98
Xiao <xiao@scp-079.org>
2019-11-10 11:26:47 +0000

    - 0.2.5

--- plugins/functions/receive.py
+++ plugins/functions/receive.py
@@ -652,7 +652,6 @@ def receive_text_data(message: Message) -> dict:
 
 def receive_user_score(project: str, data: dict) -> bool:
     # Receive and update user's score
-    glovar.locks["message"].acquire()
     try:
         # Basic data
         project = project.lower()
@@ -668,8 +667,6 @@ def receive_user_score(project: str, data: dict) -> bool:
         return True
     except Exception as e:
         logger.warning(f"Receive user score error: {e}", exc_info=True)
-    finally:
-        glovar.locks["message"].release()
 
     return False
 
