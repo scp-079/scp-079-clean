bbb5c106e1a3d3bab8e8c98c56e1471d05370b0a
Xiao <xiao@scp-079.org>
2019-12-07 14:08:19 +0000

    - 0.2.6

--- plugins/functions/tests.py
+++ plugins/functions/tests.py
@@ -53,7 +53,7 @@ def clean_test(client: Client, message: Message) -> bool:
             text += f"{lang('record_content')}{lang('colon')}{code(lang(detection))}\n"
 
         # Detected url
-        detection = is_detected_url(message)
+        detection = is_detected_url(message, True)
         if detection:
             text += f"{lang('record_link')}{lang('colon')}{code(lang(detection))}\n"
 
